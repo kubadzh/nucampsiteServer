a1affd871f115ea52e69f6ae0efa49d68ad18eeb
Kuba <dzcapital@gmail.com>
2021-03-16 12:13:12 -0400

    Basic Authentication

--- app.js
+++ app.js
@@ -34,6 +34,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Basic authentication on a server, see below
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //from()-decodes name/password
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // access is now been granted, authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
+
+// Authentication above
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
