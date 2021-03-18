85141db2b8ab5a925d2aab5549f46c4c98d65525
Kuba <dzcapital@gmail.com>
2021-03-17 22:26:58 -0400

    Express Session Part 1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session); // require f is returning another f as its return value, then we immediately calling that return f with this 2nd param list of 'session.. 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -33,30 +36,54 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-// Basic authentication on a server, see below
 
+//  app.use(cookieParser('12345-67890-09876-54321')); // there might me an issue if we use cookie=parser and express-session together
+
+
+// *** Here we use session middleware 'app.use', options below are commonly set
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false, // when new session is created, but then no updates are made to it, then at the end of the request it won't get saved, because it will just be an empty session without any usefull information and also no cookie will be sent to the client, that helps to prevent to have bunch of empty files and cookies being setup
+  resave: false, // once the seesion has bee created it will continue to be re-saved whenever request has been made to that session, even if that req didnt make any updates that needed to be saved
+  store: new FileStore() // this will create a new FileStore as an object that we use to save our session infomration to the servers hard disk, instead of just running the apps memmory 
+}));
+
+
+// Basic authentication on a server and use pf cookies,see below
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-  }
+  console.log(req.session);
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //from()-decodes name/password
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-      return next(); // access is now been granted, authorized
+  if (!req.session.user) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const user = auth[0];
+      const pass = auth[1];
+      if (user === 'admin' && pass === 'password') {
+          req.session.user = 'admin';
+          return next(); // authorized
+      } else {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
   } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      return next(err);
+      if (req.session.user === 'admin') {
+          return next();
+      } else {
+          const err = new Error('You are not authenticated!');
+          err.status = 401;
+          return next(err);
+      }
   }
 }
 
